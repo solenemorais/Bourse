dc2ed97daecb9f81ac1e7c6f9e0cdb6ee90e1dbd
Aurélien PUJOL <43996081+AurelPuj@users.noreply.github.com>
2020-05-12 19:50:43 +0200

    clean

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import time
 
 #CONSTANTS
 
-MONEY=['BTC','ETH','LTC','MLN']
+MONEY=['BTC','ETH','LTC','MLN','EOS','ATOM','DASH','WAVES']
 MONEY_SCRAPPERS=dict()
 
 def init_scrappers (element,*args):
@@ -28,7 +28,7 @@ def init_scrappers (element,*args):
     
 for element in MONEY :
     Thread(target=partial(init_scrappers,element), args=(1,)).start()
-    time.sleep(3)
+    time.sleep(4)
 
 
 #VARIABLES
@@ -36,156 +36,127 @@ for element in MONEY :
 app = tk.Tk()
 app.title('VisioCrypto')
 app.wm_iconbitmap('icon.ico')
-global_counter = 1
-df_to_plot=pd.DataFrame({'Value':[],'Date':[]})
 
-global list_plot
-global list_money_plot
+global subplot_to_display
+global money_plot
+global button_check_index
+
+button_check_index = []
 list_money_plot=[]
 
 #FONCTION
 
-
 def update_plot():
-    global global_counter
-    global list_plot
+    
+    global subplot_to_display
     global list_data_plot
-    global subplot
-    global liste
+    global button_check_index
     global MONEY_SCRAPPERS
     global list_money_plot
     
-    values=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
-    if (values.count("0") < 7):
+    checkbox_value=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
+    if (checkbox_value.count("0") < 7):
         for i,money in enumerate(list_money_plot):
-            try:
-                list_plot[i-1].lines.pop(0)
-            except:
-                print("aucune ligne")
-            
-            list_plot[i-1].plot(MONEY_SCRAPPERS[money].dataFrame.Date,MONEY_SCRAPPERS[money].dataFrame.Value,':o',color='orange')
+            if len(subplot_to_display[i-1].lines)>0:
+                subplot_to_display[i-1].lines.pop(0)
+            dataframe_money=MONEY_SCRAPPERS[money].dataFrame
+            subplot_to_display[i-1].plot(dataframe_money.Date,dataframe_money.Value,':o',color='orange')
             fig.autofmt_xdate(rotation= 45)
             canvas.draw()
     
-    elif (values.count("0") < 8):
-        try:
-            list_plot[0].lines.pop(0)
-        except:
-            print('aucune courbe à supprimer !')
-        list_plot[0].plot(MONEY_SCRAPPERS[MONEY[liste[0]]].dataFrame.Date,MONEY_SCRAPPERS[MONEY[liste[0]]].dataFrame.Value,':o',color='orange')
+    elif (checkbox_value.count("0") < 8):
+        if len(subplot_to_display[0].lines)>0:
+            subplot_to_display[0].lines.pop(0)
+        dataframe_money=MONEY_SCRAPPERS[MONEY[button_check_index[0]]].dataFrame
+        subplot_to_display[0].plot(dataframe_money.Date,dataframe_money.Value,':o',color='orange')
+        fig.autofmt_xdate(rotation= 45)
         canvas.draw()
     
 def refresh():
-    global global_counter
-    global list_test
     update_plot()
-    global_counter+=1
     app.after(500, refresh)
             
-            
-            
-#ici en fonction de la monnaie choisie on va exécuter la fonction correspondante
-#Par exemple ici si la monnaie est BTC on va exécuter la fonction BTC()    
+
+def start_stop_scrap_multiplot():
     
-
-
-def choose_money():
-    global df_to_plot
     global list_money_plot
+    global checkbox_value
     
-    values=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
+    checkbox_value=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
     
-    for i,money in enumerate(values):
+    for i,money in enumerate(checkbox_value):
         if (money == "0"):
-            if i<4 and MONEY[i] in list_money_plot:
+            if MONEY[i] in list_money_plot:
                 Thread(target=MONEY_SCRAPPERS[MONEY[i]].start_and_stop, args=(1,)).start()
-                print("\n",MONEY[i])
-                print("\n",list_money_plot)
                 list_money_plot.remove(MONEY[i])
         
         if (money != "0"):
             if MONEY[i] not in list_money_plot:
                 Thread(target=MONEY_SCRAPPERS[money].start_and_stop, args=(1,)).start()  
                 list_money_plot.append(MONEY[i])
-        
     refresh()
 
 
-global liste
-liste = []
 
-def cb():
+def get_checked_button():
     
-    global list_plot
-    global values
+    global subplot_to_display
     global fig
-    global liste
+    global button_check_index
     
     for ax in fig.get_axes():
         fig.delaxes(ax)
         
-    list_plot=[]
-    liste=[]
+    subplot_to_display=[]
+    button_check_index=[]
     
-    values=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
+    checkbox_value=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
     
-    for value in values:
+    for value in checkbox_value:
         if value!="0":
-            number_checked(value)
-        else:
-            print("box unchecked")
+            number_checked(value)   
     
-    print("----------------")
-    print (liste)
-    print("----------------")
-    
-    
-    if (len(liste)==2):
+    if (len(button_check_index)==2):
         for i in range(1,3):
-            list_plot.append(fig.add_subplot(1,2,i))
-        print(list_plot)
+            subplot_to_display.append(fig.add_subplot(1,2,i))
         
-    elif (len(liste)==1):
-        list_plot.append(fig.add_subplot(1,1,1))
-        print(list_plot)
+    elif (len(button_check_index)==1):
+        subplot_to_display.append(fig.add_subplot(1,1,1))
 
-    choose_money()
+    start_stop_scrap_multiplot()
 
 def number_checked(value):
-    global liste
-    values=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
+    global button_check_index
     
-    if (values.count("0") < 7):
-        j = values.index(value)
-        liste.append(j)
-        print(value, j)
-        if (len(liste)>1):
+    checkbox_value=[var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get(), var7.get(), var8.get()]
+    
+    if (checkbox_value.count("0") < 7):
+        j = checkbox_value.index(value)
+        button_check_index.append(j)
+        if (len(button_check_index)>1):
             for element in range(8):
-                if element not in liste:
+                if element not in button_check_index:
                     button_list[element].config(state=tk.DISABLED)
-        
                 
-    elif (values.count("0") < 8):
+    elif (checkbox_value.count("0") < 8):
         for button in button_list:
             button.config(state=tk.NORMAL)
-        liste.append(values.index(value))
-        print (value)
+        button_check_index.append(checkbox_value.index(value))
         
 #---- READ THE CSV ----
 
 fig = Figure()
+multi_plot=tk.Frame(app)
+graphic=tk.Frame(multi_plot)
 
-frame_multi_plot=tk.Frame(app)
-frame_plot=tk.Frame(frame_multi_plot)
 
-
-canvas = FigureCanvasTkAgg(fig, master=frame_plot)  # A tk.DrawingArea.
+canvas = FigureCanvasTkAgg(fig, master=graphic)  # A tk.DrawingArea.
 canvas.get_tk_widget().pack(fill="both", expand=True)
-frame_plot.pack(fill='both',side='left',expand=True)
+graphic.pack(fill='both',side='left',expand=True)
 
 
 #on crée une Frame qui va contenir les bouttons
-frame_button=tk.Frame(frame_multi_plot)
+button_widget=tk.Frame(multi_plot)
 
 var1=tk.StringVar()
 var2=tk.StringVar()
@@ -197,14 +168,14 @@ var7=tk.StringVar()
 var8=tk.StringVar()
 
 #On défnit chaque boutton pour chaque monnaie avec comme paramètre le nom de la monnaie 
-button_btc = tk.Checkbutton(frame_button, text='Bitcoin', variable=var1, onvalue="BTC", command=cb)
-button_eth = tk.Checkbutton(frame_button, text='Ethereum', variable=var2, onvalue="ETH", command=cb)
-button_ltc = tk.Checkbutton(frame_button, text='Litecoin', variable=var3, onvalue="LTC", command=cb)
-button_mon = tk.Checkbutton(frame_button, text='Monero', variable=var4, onvalue="MLN", command=cb)
-button_rip = tk.Checkbutton(frame_button, text='EOS', variable=var5, onvalue="EOS", command=cb)
-button_car = tk.Checkbutton(frame_button, text='ATOM', variable=var6, onvalue="ATOM", command=cb)
-button_das = tk.Checkbutton(frame_button, text='Dash', variable=var7, onvalue="Dash", command=cb)
-button_lib = tk.Checkbutton(frame_button, text='WAVES', variable=var8, onvalue="Waves", command=cb)
+button_btc = tk.Checkbutton(button_widget, text='Bitcoin', variable=var1, onvalue="BTC", command=get_checked_button)
+button_eth = tk.Checkbutton(button_widget, text='Ethereum', variable=var2, onvalue="ETH", command=get_checked_button)
+button_ltc = tk.Checkbutton(button_widget, text='Litecoin', variable=var3, onvalue="LTC", command=get_checked_button)
+button_mon = tk.Checkbutton(button_widget, text='Monero', variable=var4, onvalue="MLN", command=get_checked_button)
+button_rip = tk.Checkbutton(button_widget, text='EOS', variable=var5, onvalue="EOS", command=get_checked_button)
+button_car = tk.Checkbutton(button_widget, text='ATOM', variable=var6, onvalue="ATOM", command=get_checked_button)
+button_das = tk.Checkbutton(button_widget, text='DASH', variable=var7, onvalue="DASH", command=get_checked_button)
+button_lib = tk.Checkbutton(button_widget, text='WAVES', variable=var8, onvalue="WAVES", command=get_checked_button)
 button_list = [button_btc, button_eth, button_ltc, button_mon, button_rip, button_car, button_das, button_lib]
 
 for button in button_list:
@@ -213,8 +184,9 @@ for button in button_list:
     button.pack(fill='both',expand=True,side="top")
 
 
-frame_button.pack(fill='y',side="right")
-frame_multi_plot.pack(fill='both',expand=True)
+button_widget.pack(fill='y',side="right")
+multi_plot.pack(fill='both',expand=True)
+
 app.mainloop()
 
 for scrapper in MONEY:
